d1f61b3b8ffcf555ef2fd7811d1f92c23a6d54f4
Vinay Shenoy <vinay@obvious.in>
2020-05-12 13:36:14 +0530

    Initial commit

---
+++ app/src/main/java/in/obvious/android/starter/MainActivity.kt
@@ -0,0 +1,12 @@
+package `in`.obvious.android.starter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
