24ff01d2573503b65ee8ebf13112b1859eb67f5e
Vinay Shenoy <vinay@obvious.in>
2020-05-14 10:36:08 +0530

    Extract interface from the `Countdown` class
    This is being done in order to make the class usable for tests.

---
+++ app/src/main/java/in/obvious/android/starter/counter/PlatformCountdown.kt
@@ -0,0 +1,49 @@
+package `in`.obvious.android.starter.counter
+
+import android.os.CountDownTimer
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeUnit.MILLISECONDS
+import java.util.concurrent.TimeUnit.SECONDS
+
+class PlatformCountdown : Countdown {
+
+    private lateinit var timer: CountDownTimer
+
+    override fun start(
+        tickAmount: Int,
+        tickTimeUnit: TimeUnit,
+        tick: () -> Unit,
+        done: () -> Unit
+    ) {
+        timer = createTimer(tickAmount, tickTimeUnit, done, tick)
+
+        timer.start()
+    }
+
+    private fun createTimer(
+        tickAmount: Int,
+        tickTimeUnit: TimeUnit,
+        done: () -> Unit,
+        tick: () -> Unit
+    ): CountDownTimer {
+        return object : CountDownTimer(
+            MILLISECONDS.convert(tickAmount.toLong(), tickTimeUnit),
+            MILLISECONDS.convert(1L, SECONDS)
+        ) {
+
+            override fun onFinish() {
+                done()
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                tick()
+            }
+        }
+    }
+
+    override fun cancel() {
+        if (::timer.isInitialized) {
+            timer.cancel()
+        }
+    }
+}
