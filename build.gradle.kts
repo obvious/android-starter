22c5dc27cb7fd6c9d717666a4f03ebf936dea277
Harsh Shandilya <harsh@obvious.in>
2022-03-11 11:40:54 +0530

    build: restrict version update checks to stable channel
    Signed-off-by: Harsh Shandilya <harsh@obvious.in>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,24 @@
 @file:Suppress("DSL_SCOPE_VIOLATION", "UnstableApiUsage")
+
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
   alias(libs.plugins.spotless)
   alias(libs.plugins.versions)
   alias(libs.plugins.vcu)
+}
+
+fun isNonStable(version: String): Boolean {
+  val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+  val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+  val isStable = stableKeyword || regex.matches(version)
+  return isStable.not()
+}
+
+tasks.withType<DependencyUpdatesTask> {
+  rejectVersionIf { isNonStable(candidate.version) && !isNonStable(currentVersion) }
+  checkForGradleUpdate = false
+  checkBuildEnvironmentConstraints = true
 }
 
 spotless {
